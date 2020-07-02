323b461725f0037fc11b5eb4af5750a15937d70e
sammyhy <sammykkoon@gmail.com>
2020-07-02 17:14:15 +0900

    Updates

---
+++ precache-manifest.fd786f95fa315e2fb81f47b00b99a236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3587d2c04d52f7ef2df4150611b829",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "62cfaecbe80cce777a3e",
+    "url": "/movie_app_2020/static/css/main.be35b40a.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62cfaecbe80cce777a3e",
+    "url": "/movie_app_2020/static/js/main.2f134167.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
